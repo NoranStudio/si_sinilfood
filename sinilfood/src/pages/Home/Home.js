c5786a2891548dd95a218fc9a1a06848342d636e
Your GitHub Username <noranstudio0707@gmail.com>
2025-01-08 06:40:21 +0900

    add : counsel section + footer.js - footer 분리작업 완료

--- sinilfood/src/pages/Home/Home.js
+++ sinilfood/src/pages/Home/Home.js
@@ -27,7 +27,7 @@ import rightbutton from '../../assets/img/right_button.png';
 import counsel_image from '../../assets/img/counsel_image.png'; // 상담 이미지
 
 
-
+import Footer from "../../components/Footer"; //footer
 
 // 프랜차이즈 슬라이더 컴포넌트 가져오기
 import FranchiseSlider from './FranchiseSlider.js';
@@ -188,34 +188,48 @@ function App() {
       </div>
 
       {/* 상담센터 섹션 */}
-      <div className="contact-center">
-        <div className="contact-content">
-          <h2 className="contact-title">
+      <div className="counsel-center">
+        <div className="counsel-content">
+          <h2 className="counsel-title">
             협력사 사업문의의 <span className="highlight">상담센터</span>
           </h2>
-          <p className="contact-description">
+          <p className="counsel-description">
             신일푸드는 함께 성장할 파트너사들을 기다립니다. <br />
             식용유 구매와 제조관련 상담문의를 남겨주세요.
           </p>
-          <button className="contact-button">
+          <button className="counsel-button">
             온라인 상담문의
-            <span className="arrow">&gt;</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="22"
+              viewBox="0 0 12 22"
+              fill="none"
+              className="arrow-svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0.414002 20.9111C0.948548 21.4501 1.81037 21.4501 2.34491 20.9111L11.4104 11.7701C11.8358 11.3411 11.8358 10.6481 11.4104 10.2191L2.34491 1.07808C1.81037 0.539078 0.948548 0.539078 0.414002 1.07808C-0.120543 1.61708 -0.120543 2.48608 0.414002 3.02508L8.31218 11.0001L0.403093 18.9751C-0.120544 19.5031 -0.120543 20.3831 0.414002 20.9111Z"
+                fill="white"
+              />
+            </svg>
           </button>
-          <p className="contact-info">
+          <p className="counsel-info">
             평일 AM 09:00 - PM 17:00 <br />
             <span className="phone-number">055. 334. 7789</span>
           </p>
         </div>
-        <div className="contact-image">
+        <div className="counsel-image">
           <img
             src={counsel_image}
             alt="상담 이미지"
-            className="contact-image-file"
+            className="counsel-image-file"
           />
         </div>
-
-        
       </div>
+      {/* Footer */}
+      <Footer />
   </div>
   
 
