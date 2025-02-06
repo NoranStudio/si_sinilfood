fa79ed0ac2ce92751bdf19642f9e238a84ee93ad
leonheart0910 <wbs090801@gmail.com>
2025-02-06 19:58:03 +0900

    separate : banner section

--- sinilfood/src/pages/Home/home_banner.jsx
+++ sinilfood/src/pages/Home/home_banner.jsx
@@ -5,7 +5,9 @@ import phoneIcon from "../../assets/img/phone.png";
 import headsetIcon from "../../assets/img/headset.png";
 import banner from "../../assets/img/banner.png";
 
-const Section1 = () => {
+import "./home_css/banner.css";
+
+const home_banner = () => {
   return (
     <div
       style={{
@@ -135,58 +137,9 @@ const Section1 = () => {
             </div>
           </div>
         </div>
-
-        {/* <div className="logo">
-        <img src={logo} alt="Sinilfood Logo" />
-      </div> */}
-
-        {/* 네비게이션 바 */}
-        {/* <nav className="navbar">
-        <ul>
-          <li className="nav-item company">
-            <a href="#about">회사소개</a>
-          </li>
-          <li className="nav-item business">
-            <a href="#business">사업영역</a>
-          </li>
-          <li className="nav-item product">
-            <a href="#products">제품소개</a>
-          </li>
-          <li className="nav-item service">
-            <a href="#services">고객서비스</a>
-          </li>
-        </ul>
-      </nav> */}
-
-        {/* SHINLI FOOD 이미지 */}
-        {/* <img
-        src={shinliFoodImage}
-        alt="SHINLI FOOD"
-        className="shinli-food-img"
-      /> */}
-
-        {/* 배너 텍스트 배치 */}
-        {/* <h1 className="title-large">식용유 및 식품 유통</h1>
-      <h1 className="title-large">
-        전문기업 <span className="highlight">신일푸드</span>
-      </h1>
-      <p className="description">
-        <strong>신일푸드</strong> 만의 독자적인 전용 물류 시스템으로 전국적 유통
-        체인망이 구축되어 있습니다.
-      </p> */}
-
-        {/* 우측 사이드 버튼 */}
-        {/* <div className="side-buttons">
-        <a href="#contact" className="contact-button">
-          <img src={phoneIcon} alt="Phone Icon" className="icon-image" />
-        </a>
-        <a href="#contact" className="contact-button">
-          <img src={headsetIcon} alt="Headset Icon" className="icon-image" />
-        </a>
-      </div> */}
       </div>
     </div>
   );
 };
 
-export default Section1;
+export default home_banner;
