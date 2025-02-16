224dd8595cbfc0c99e7cae42f87ff253f87e343e
hjkim0905 <eax9952@gmail.com>
2025-02-16 20:21:00 +0900

    [Feat] : 사업영역 -> 전용유 제작 페이지 최종1차완료

--- sinilfood/src/components/business/Manufacturing.js
+++ sinilfood/src/components/business/Manufacturing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Business.css';
 import oilFeature from '../../assets/img/business/oil_feature.png';
+import roleGraph from '../../assets/img/business/role_graph.png';
 
 function Manufacturing() {
     return (
@@ -33,6 +34,82 @@ function Manufacturing() {
                         <br />
                         역시 일반유보다 향상되어 맛의 균일화가 가능합니다.
                     </p>
+                </div>
+                <div className="backline"></div>
+                <div className="advantages-section">
+                    <h3 className="advantages-title advantages-title-multiline">
+                        전용유
+                        <br />
+                        활용시 장점
+                    </h3>
+                    <div className="advantages-boxes">
+                        <div className="advantage-box">
+                            <h4 className="advantage-box-title">지속적 관리 & 수익 안정화</h4>
+                            <p className="advantage-box-description">
+                                가맹점에 대한 지속적인 관리와 안정적인 수익구조 구축
+                            </p>
+                        </div>
+                        <div className="advantage-box">
+                            <h4 className="advantage-box-title">차별성</h4>
+                            <p className="advantage-box-description">
+                                프랜차이즈 가맹점의 차별화된 재고관리와 소비자에게 신뢰를 줄수있는 대기업 제조사와의
+                                전용유 제작으로 인해 안정적인 생산과 공급망 구축과 경쟁력 있는 가격 으로 인한 지속가능한
+                                수익구조 완성
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="backline-bottom"></div>
+                <div className="advantages-section">
+                    <h3 className="advantages-title">
+                        (주)신일푸드
+                        <br />
+                        선정시 장점
+                    </h3>
+                    <div className="advantages-boxes">
+                        <div className="advantage-box">
+                            <h4 className="advantage-box-title">물류</h4>
+                            <ul className="advantage-box-list">
+                                <li className="advantage-box-description">
+                                    전용 물류시스템 사용으로 "물류 효율화" 상승 가능
+                                    <br />
+                                    (기존 물류 이용시 1CAN당 최소 4,000원 비용 발생)
+                                </li>
+                                <li className="advantage-box-description">
+                                    신일푸드 물류시스템을 관리의 툴로 이용가능
+                                    <br />
+                                    (관리하기 예민한 부분인 전용유 사용 여부를 신일푸드가 직접 관리 가능)
+                                </li>
+                                <li className="advantage-box-description">
+                                    전국적 유통 체인망이 구축되어 있어 상기 장점을 지방까지 적용 가능
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="advantage-box">
+                            <h4 className="advantage-box-title">고객사 자금운용</h4>
+                            <p className="advantage-box-description">
+                                기존 전용유 매입에 대한 자금을 신일푸드에서 대신 매입함으로써 고객사 자금 운영의 여유가
+                                생겨 향후 고객사의 다른 투자 및 자금 운용에 이점이 발생함
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="backline-bottom"></div>
+                <div className="advantages-section">
+                    <h3 className="advantages-title">주체별 역할</h3>
+                    <div className="role-content-section">
+                        <div className="role-description">
+                            <span className="role-text">지속적 관리와 신속한 대응으로 </span>
+                            <span className="role-text-bold">가맹점 만족도 상승</span>
+                        </div>
+                        <img src={roleGraph} alt="주체별 역할 그래프" className="role-graph" />
+                    </div>
+                </div>
+                <div className="backline-bottom"></div>
+                <div className="advantages-section">
+                    <h3 className="advantages-title" style={{ whiteSpace: 'nowrap' }}>
+                        본사물류의 한계
+                    </h3>
                 </div>
             </div>
         </div>
