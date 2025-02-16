f4c3fab7cc20fd7cefd4acbbaa779996bde9ffd6
hjkim0905 <eax9952@gmail.com>
2025-02-16 20:27:43 +0900

    [Feat] : Footer added

--- sinilfood/src/App.js
+++ sinilfood/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppRoutes from './routes';
+import Footer from './components/Footer';
 import './assets/styles/global.css';
 import './assets/styles/fonts.css';
 
@@ -7,6 +8,7 @@ const App = () => {
     return (
         <div className="App">
             <AppRoutes />
+            <Footer />
         </div>
     );
 };
