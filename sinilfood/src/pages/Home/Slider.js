d8111cd7e46660c2c79f52efc7d8a0f8e28e1f31
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2025-02-23 13:05:38 +0900

    feat : 프렌차이즈섹션

--- sinilfood/src/pages/Home/Slider.js
+++ sinilfood/src/pages/Home/Slider.js
@@ -1,57 +1,58 @@
-//프랜차이즈 로고 블러 이미지
 import franchise1Logo from "../../assets/img/franchise/franchise1.png";
 import franchise2Logo from "../../assets/img/franchise/franchise2.png";
 import franchise3Logo from "../../assets/img/franchise/franchise3.png";
 import franchise4Logo from "../../assets/img/franchise/franchise4.png";
 import franchise5Logo from "../../assets/img/franchise/franchise5.png";
 
-import React, { useEffect, useRef } from "react";
+import "../../assets/styles/slider.css";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function Slider() {
-  const sliderRef = useRef(null);
+// Import Swiper styles
+import "swiper/css";
 
-  useEffect(() => {
-    const slider = sliderRef.current;
-    let scrollAmount = 0;
-
-    const slideInterval = setInterval(() => {
-      if (slider) {
-        scrollAmount += slider.offsetWidth / 5; // 한 번에 이동할 거리
-        if (scrollAmount >= slider.scrollWidth) {
-          scrollAmount = 0; // 다시 처음으로 이동
-        }
-        slider.scrollTo({
-          left: scrollAmount,
-          behavior: "smooth", // 부드럽게 스크롤
-        });
-      }
-    }, 1000); // 1초 간격
-
-    return () => clearInterval(slideInterval); // 컴포넌트 언마운트 시 인터벌 제거
-  }, []);
-
+const Slider = () => {
   return (
-    <div className="franchise-section">
-      <h3 className="partners-title">프랜차이즈 물류업체</h3>
-      <div className="franchise-slider" ref={sliderRef}>
-        <div className="franchise-item">
+    <div className="slider-section">
+      <h3>프랜차이즈 물류업체</h3>
+      <Swiper
+        spaceBetween={24}
+        slidesPerView="auto"
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
           <img src={franchise1Logo} alt="프랜차이즈 1" />
-        </div>
-        <div className="franchise-item">
+        </SwiperSlide>
+        <SwiperSlide>
           <img src={franchise2Logo} alt="프랜차이즈 2" />
-        </div>
-        <div className="franchise-item">
+        </SwiperSlide>
+        <SwiperSlide>
           <img src={franchise3Logo} alt="프랜차이즈 3" />
-        </div>
-        <div className="franchise-item">
+        </SwiperSlide>
+        <SwiperSlide>
           <img src={franchise4Logo} alt="프랜차이즈 4" />
-        </div>
-        <div className="franchise-item">
+        </SwiperSlide>
+        <SwiperSlide>
           <img src={franchise5Logo} alt="프랜차이즈 5" />
-        </div>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={franchise1Logo} alt="프랜차이즈 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={franchise2Logo} alt="프랜차이즈 2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={franchise3Logo} alt="프랜차이즈 3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={franchise4Logo} alt="프랜차이즈 4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={franchise5Logo} alt="프랜차이즈 5" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
-}
+};
 
 export default Slider;
